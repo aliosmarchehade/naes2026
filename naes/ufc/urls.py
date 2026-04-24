008c37026d4cefd7f7567f1e11e5dda406990872
ali osmar <ali.osmar.chehade@gmail.com>
2026-04-24 20:58:05 -0300

    form e list

--- naes/ufc/urls.py
+++ naes/ufc/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('lutadores/', LutadorList.as_view(), name='lutador-list'),
+    path('lutadores/novo/', LutadorCreate.as_view(), name='lutador-create'),
+    path('lutadores/<int:pk>/editar/', LutadorUpdate.as_view(), name='lutador-update'),
+    path('lutadores/<int:pk>/excluir/', LutadorDelete.as_view(), name='lutador-delete'),
+    path('lutadores/<int:pk>/', LutadorDetail.as_view(), name='lutador-detail'),
 ]
--- naes/ufc/views.py
+++ naes/ufc/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
+from django.urls import reverse_lazy
+from .models import Lutador
 
-# Create your views here.
+
+# CREATE
+class LutadorCreate(CreateView):
+    model = Lutador
+    fields = ['nome', 'apelido', 'idade', 'peso', 'categoria']
+    template_name = 'form.html'
+    success_url = reverse_lazy('lutador-list')
+
+    extra_context = {
+        'titulo': 'Cadastro de Lutador',
+        'botao': 'Cadastrar'
+    }
+
+    def form_valid(self, form):
+        form.instance.cadastrado_por = self.request.user
+        return super().form_valid(form)
+
+
+# UPDATE
+class LutadorUpdate(UpdateView):
+    model = Lutador
+    fields = ['nome', 'apelido', 'idade', 'peso', 'categoria', 'vitorias', 'derrotas']
+    template_name = 'form.html'
+    success_url = reverse_lazy('lutador-list')
+
+    extra_context = {
+        'titulo': 'Editar Lutador',
+        'botao': 'Atualizar'
+    }
+
+
+# DELETE
+class LutadorDelete(DeleteView):
+    model = Lutador
+    template_name = 'confirm_delete.html'
+    success_url = reverse_lazy('lutador-list')
+
+    extra_context = {
+        'titulo': 'Excluir Lutador',
+        'botao': 'Confirmar Exclusão'
+    }
+
+
+# LIST
+class LutadorList(ListView):
+    model = Lutador
+    template_name = 'lutador_list.html'
+    context_object_name = 'lutadores'
+    paginate_by = 10
+
+
+# DETAIL
+class LutadorDetail(DetailView):
+    model = Lutador
+    template_name = 'lutador_detail.html'
